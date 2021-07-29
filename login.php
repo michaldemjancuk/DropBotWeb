d9c2982a73af1aed8aeb972eb6d9ec132ff4c4e5
Michal Demjančuk <demjancukmichal@gmail.com>
2021-07-29 16:06:02 +0200

    Update links to dynamic (bootstrap)

--- login.php
+++ login.php
@@ -91,22 +91,17 @@ function FirstLogin()
 ?>
 
 <!DOCTYPE html>
-<html lang="cs">
+<html lang="cs" style="height: 100%">
 <head>
 	<title>Port&aacute;l &uacute;dr&#382;by</title>
     <meta charset="utf-8" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap/bootstrap.css"/>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap-grid.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap-reboot.css">
-	<link rel="stylesheet" type="text/css" href="/css/custom.css">
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/jquery-3.3.1.js"></script>
-    <script src="js/modernizr-2.8.3.js"></script>
-    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="/css/custom.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="js/custom.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript">
     	function validate(){
 			if ($('#register_password').val() != $('#register_password2').val()
@@ -127,68 +122,88 @@ function FirstLogin()
     </script>
 </head>
 
-<body>
-    <div class="container">
-        <div class="row">
-			<div class="col-md-5 mx-auto jumbotron">
-				<div class="form">
-					<div class="logo mb-3">
-						<div class="col-md-12 text-center">
-							<h1>P&#345;ihl&aacute;&#353;en&iacute;</h1>
-						</div>
+<body class="bg-secondary" style="height: 100% !important">
+	<div class="mx-auto col-md-6 position-absolute top-50 start-50 translate-middle">
+		<ul class="nav nav-tabs" id="myTab" role="tablist">
+			<li class="nav-item" role="presentation">
+				<button class="nav-link active bg-white" id="login-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Login</button>
+			</li>
+			<li class="nav-item" role="presentation">
+				<button class="nav-link bg-white" id="registration-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Registration</button>
+			</li>
+		</ul>
+		<div class="tab-content bg-white rounded" id="myTabContent">
+			<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="login-tab">
+				<div class="container">
+					<div class="row">
+						
+						<span class="spacer-25"></span>
+
+						<form class="mb-3">
+							<div class="row mb-3">
+								<label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
+								<div class="col-sm-10">
+									<input type="email" class="form-control" id="inputEmail3">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+								<div class="col-sm-10">
+									<input type="password" class="form-control" id="inputPassword3">
+								</div>
+							</div>
+							<button type="submit" class="btn btn-primary">Sign in</button>
+						</form>
+
 					</div>
-					<hr />
-					<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="login">
-						<div class="form-group">
-						  <label>&#268;&iacute;slo zam&#283;stnance</label>
-						  <input type="number" name="id"  class="form-control" id="login_id" placeholder="Osobní číslo zaměstnance" min="10000" max="99999" value="10000" required>
-						</div>
-						<div class="form-group">
-							<label>Heslo</label>
-							<input type="password" name="password" id="login_password"  class="form-control" placeholder="Heslo" required>
-						</div>
-						<input type="text" name="type" value="login" hidden>
-						<button type="submit" class="form-group btn btn-primary">P&#345;ihl&aacute;sit</button>
-                    </form>
 				</div>
 			</div>
-			<div class="col-md-1">
-				
-			</div>
-			<div class="col-md-5 mx-auto jumbotron">
-				<div class="form">
-					<div class="logo mb-3">
-						<div class="col-md-12 text-center">
-							<h1>Prvn&iacute; p&#345;ihl&aacute;&#353;en&iacute; (Registrace)</h1>
-							<!-- <hr />
-							 <h1>(&Uacute;&#269;et bez hesla)</h1> -->
-						</div>
+
+			<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="registration-tab">
+				<div class="container">
+					<div class="row">
+						
+						<span class="spacer-25"></span>
+
+						<form class="mb-3">
+							<div class="row mb-3">
+								<label for="username" class="col-sm-2 col-form-label">Username</label>
+								<div class="col-sm-10">
+									<input type="username" class="form-control" id="username">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<label for="email" class="col-sm-2 col-form-label">Email</label>
+								<div class="col-sm-10">
+									<input type="email" class="form-control" id="email">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<label for="password" class="col-sm-2 col-form-label">Password</label>
+								<div class="col-sm-10">
+									<input type="password" class="form-control" id="password">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<label for="password2" class="col-sm-2 col-form-label"></label>
+								<div class="col-sm-10">
+									<input type="password" class="form-control" id="password2">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<label for="birth" class="col-sm-2 col-form-label">Birth date</label>
+								<div class="col-sm-10">
+									<input type="date"  class="form-control" id="birth">
+								</div>
+							</div>
+							<button type="submit" class="btn btn-primary">Sign in</button>
+						</form>
+
 					</div>
-					<hr />
-					<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="login">
-						<div class="form-group">
-						  <label>&#268;&iacute;slo zam&#283;stnance</label>
-						  <input type="number" name="id"  class="form-control" id="register_id" placeholder="Osobní číslo zaměstnance" min="10000" max="99999" value="10000" required>
-						</div>
-						<div class="form-group">
-						  <label>Jm&eacute;no zam&#283;stnance</label>
-						  <input type="text" name="fName"  class="form-control" id="register_fname" placeholder="Jm&eacute;no" maxlength="50" required>
-						</div>
-						<div class="form-group">
-						  <label>P&#345;&iacute;jmen&iacute; zam&#283;stnance</label>
-						  <input type="text" name="lName"  class="form-control" id="register_lname" placeholder="P&#345;&iacute;jmen&iacute;" maxlength="50" required>
-						</div>
-						<div class="form-group">
-							<label>2x Heslo (Pro ov&#283;&#345;en&iacute; spr&aacute;vnosti zad&aacute;n&iacute;)</label>
-							<input type="password" name="password" id="register_password"  class="form-control" placeholder="Heslo">
-							<input type="password" name="password2" id="register_password2"  class="form-control" placeholder="Heslo">
-						</div>
-						<input type="text" name="type" value="reset" hidden>
-                    	<button type="submit" onclick="return validate()" class="form-group btn btn-info">Nastavit heslo</button>
-                    </form>
 				</div>
 			</div>
 		</div>
-	</div>  
+	</div>
 </body>
+
 </html>
