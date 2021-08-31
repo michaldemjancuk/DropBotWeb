9829ea266a75707022088d1e8fc3354d77ac193d
Michal Demjančuk <demjancukmichal@gmail.com>
2021-08-31 02:21:20 +0200

    Processing image fixes

--- actions/dbot_processImage.php
+++ actions/dbot_processImage.php
@@ -15,44 +15,76 @@ $template = $_POST["template"];
 $permissionRole = $_POST["permissionRole"];
 $dbot_dbId = 1;
 $emj = "❤";
-
-$processedImgName = bin2hex(openssl_random_pseudo_bytes(16)) . ".png";
-
+$processedImgName = "";
 $dirnameThis = dirname(__FILE__);
-$root = substr($dirnameThis, 0, strrpos( $dirnameThis, '/'));
-$target_dir = $root . "/dropImageUploadsProcessed/";
-$target_file = $target_dir . $processedImgName;
-$nameWithFolder =  "dropImageUploadsProcessed/" . $processedImgName;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 $imgGenerator = new ImgGenerator();
 $dbot_diu = new DropImageUploads();
 
 $unqUpl = $dbot_diu->GetAllUniqueUploadsInPermGroup($permissionRole);
+$uniqueUsers = $dbot_diu->GetAllUniqueUsersWithPhotoInPermGroup($permissionRole);
 
-$imgGenerator->
-  Combine4by2_Basic($unqUpl[0]['LocalUrl'],$unqUpl[1]['LocalUrl'],$unqUpl[2]['LocalUrl'],$unqUpl[3]['LocalUrl'],$unqUpl[4]['LocalUrl'],$unqUpl[5]['LocalUrl'],$unqUpl[6]['LocalUrl'],$unqUpl[7]['LocalUrl'], $processedImgName);
+shuffle($unqUpl);
 
-$description =  
-  $emj . $unqUpl[0]['Username'] . $emj . 
-  $unqUpl[1]['Username'] . $emj . 
-  $unqUpl[2]['Username'] . $emj . 
-  $unqUpl[3]['Username'] . $emj . 
-  $unqUpl[4]['Username'] . $emj . 
-  $unqUpl[5]['Username'] . $emj . 
-  $unqUpl[6]['Username'] . $emj . 
-  $unqUpl[7]['Username'] . $emj;
+$splittedUsers = SplitUsersIntoArrays($unqUpl);
 
-echo $description . "<br>";
-echo $nameWithFolder . "<br>";
-echo $dbot_dbId . "<br>";
+for ($i=0; $i < (count($splittedUsers) < $dropsCount ? $dropsCount : count($splittedUsers)) ; $i++) { 
+  $processedImgName = bin2hex(openssl_random_pseudo_bytes(16)) . ".png";
+  $root = substr($dirnameThis, 0, strrpos( $dirnameThis, '/'));
+  $target_dir = $root . "/dropImageUploadsProcessed/";
+  $target_file = $target_dir . $processedImgName;
+  $nameWithFolder =  "dropImageUploadsProcessed/" . $processedImgName;
+  $imageUrls = array_fill(0, 8, "");
+  $description = $emj;
+  for ($y=0; $y < count($splittedUsers[$i]); $y++) { 
+    $description .= $splittedUsers[$i][$y]['Username'] . $emj;
+    $imageUrls[$y] = $splittedUsers[$i][$y]['LocalUrl'];
+  }
+  $imgGenerator->
+    Combine4by2_Basic($imageUrls, $processedImgName);
 
-$dbot_ImgGen = new Dbot_ImgGen();
+  echo $description . "<br>";
+  echo $nameWithFolder . "<br>";
+  echo $dbot_dbId . "<br>";
 
-$dbot_ImgGen->Add($nameWithFolder, $description, $dbot_dbId);
+  $dbot_ImgGen = new Dbot_ImgGen();
+
+  $dbot_ImgGen->Add($nameWithFolder, $description, $dbot_dbId);
+}
+
+// $unqUploads = array($unqUpl[0]['LocalUrl'],$unqUpl[1]['LocalUrl'],$unqUpl[2]['LocalUrl'],$unqUpl[3]['LocalUrl'],$unqUpl[4]['LocalUrl'],$unqUpl[5]['LocalUrl'],$unqUpl[6]['LocalUrl'],$unqUpl[7]['LocalUrl']);
+
+// $imgGenerator->
+//   Combine4by2_Basic_2($unqUploads, $processedImgName);
+
+
 
 // echo $msg;
 
-header('Location: /generatedDrops.php'); 
+//header('Location: /generatedDrops.php'); 
+
+
+function SplitUsersIntoArrays($users, $divideBy=8)
+{
+  $auth = new Authenticator();
+  $input      = $users;
+  $chunk_size = $divideBy;
+  $output     = array_chunk($input, $chunk_size);
+
+  // If we can't chunk into equal sized parts merge the last two arrays.
+  if(count($input) % $chunk_size) {
+    $leftovers = array_pop($output);
+    $last      = array_pop($output);
+    array_push($output, array_merge($last, $leftovers));
+  }
+
+  if($auth->IsSuperAdmin()){
+    // echo "<pre>";
+    // var_export($output);
+    // echo "</pre>";
+  }
+
+  return $output;
+}
 
 ?>
--- libs/custom/imgGen.php
+++ libs/custom/imgGen.php
@@ -10,106 +10,47 @@ class ImgGenerator
 	{
 	}
 
-	public function Combine4by2_Basic($img1, $img2, $img3, $img4, $img5, $img6, $img7, $img8, $imgOutput = 'test.png')
+	public function Combine4by2_Basic($images, $imgOutput = 'test.png')
 	{
 		$imgOutput = __DIR__.'/../../dropImageUploadsProcessed/'.$imgOutput;
 
 		echo $imgOutput;
 
 		$homeUrl = AllSettings()['HomepageURL'];
-		$img1 = $homeUrl . $img1;
-		$img2 = $homeUrl . $img2;
-		$img3 = $homeUrl . $img3;
-		$img4 = $homeUrl . $img4;
-		$img5 = $homeUrl . $img5;
-		$img6 = $homeUrl . $img6;
-		$img7 = $homeUrl . $img7;
-		$img8 = $homeUrl . $img8;
 
-		list($wdth_1, $hght_1) = getimagesize($img1);
-		list($wdth_2, $hght_2) = getimagesize($img2);
-		list($wdth_3, $hght_3) = getimagesize($img3);
-		list($wdth_4, $hght_4) = getimagesize($img4);
-		list($wdth_5, $hght_5) = getimagesize($img5);
-		list($wdth_6, $hght_6) = getimagesize($img6);
-		list($wdth_7, $hght_7) = getimagesize($img7);
-		list($wdth_8, $hght_8) = getimagesize($img8);
+		for ($i=0; $i < 8; $i++) { 
+			$images[$i] = $homeUrl . $images[$i];
+		}
+		$sizeX = 1280;
+		$sizeY = 1280;
 
-		echo "<br>wdth: " . $wdth_1 . "hght: " . $hght_1;
-		echo "<br>wdth: " . $wdth_2 . "hght: " . $hght_2; 
-		echo "<br>wdth: " . $wdth_3 . "hght: " . $hght_3; 
-		echo "<br>wdth: " . $wdth_4 . "hght: " . $hght_4; 
-		echo "<br>wdth: " . $wdth_5 . "hght: " . $hght_5; 
-		echo "<br>wdth: " . $wdth_6 . "hght: " . $hght_6; 
-		echo "<br>wdth: " . $wdth_7 . "hght: " . $hght_7; 
-		echo "<br>wdth: " . $wdth_8 . "hght: " . $hght_8 . "<br>";
-
-		$wdths = array(
-			$img1=>$wdth_1,
-			$img2=>$wdth_2, 
-			$img3=>$wdth_3, 
-			$img4=>$wdth_4, 
-			$img5=>$wdth_5, 
-			$img6=>$wdth_6, 
-			$img7=>$wdth_7, 
-			$img8=>$wdth_8);
-
-		asort($wdths);
-
-		$wdthsKeys = array_keys($wdths);
-		$wdthsValues = array_values($wdths);
-
-		list($sizeX, $sizeY) = $this->GetNewImageDimensions(
-			$wdthsKeys[7],
-			$wdthsKeys[5],
-			$wdthsKeys[3],
-			$wdthsKeys[1],
-			$wdthsKeys[0],
-			$wdthsKeys[7],
-			$wdthsKeys[4],
-			$wdthsKeys[6],
-		);
 		echo "X: " . $sizeX . " | Y: " . $sizeY;
 
 		// Create empty image with size prepared
 	 	$image = imagecreatetruecolor($sizeX, $sizeY);
 
-		$image1 = imagecreatefrompng($wdthsKeys[7]);
-		$image2 = imagecreatefrompng($wdthsKeys[5]);
-		$image3 = imagecreatefrompng($wdthsKeys[3]);
-		$image4 = imagecreatefrompng($wdthsKeys[1]);
-		$image5 = imagecreatefrompng($wdthsKeys[0]);
-		$image6 = imagecreatefrompng($wdthsKeys[2]);
-		$image7 = imagecreatefrompng($wdthsKeys[4]);
-		$image8 = imagecreatefrompng($wdthsKeys[6]);
+	 	$pngImages = array_fill(0, 8, "");
 
-		list($width_1, $height_1) = getimagesize($wdthsKeys[7]);
-		list($width_2, $height_2) = getimagesize($wdthsKeys[5]);
-		list($width_3, $height_3) = getimagesize($wdthsKeys[3]);
-		list($width_4, $height_4) = getimagesize($wdthsKeys[1]);
-		list($width_5, $height_5) = getimagesize($wdthsKeys[0]);
-		list($width_6, $height_6) = getimagesize($wdthsKeys[2]);
-		list($width_7, $height_7) = getimagesize($wdthsKeys[4]);
-		list($width_8, $height_8) = getimagesize($wdthsKeys[6]);
-
-		$heights = array($height_1, $height_2, $height_3, $height_4, $height_5, $height_6, $height_7, $height_8);
-
-		$wdthsLine1 = $wdthsValues[7] + $wdthsValues[5] + $wdthsValues[3] + $wdthsValues[1];
-		$wdthsLine2 = $wdthsValues[0] + $wdthsValues[2] + $wdthsValues[4] + $wdthsValues[6];
-
-		$wdthsLineDiff = abs($wdthsLine1 - $wdthsLine2);
+		for ($i=0; $i < 8; $i++) { 
+			echo $images[$i];
+			$pngImages[$i] = imagecreatefrompng($images[$i]);
+		}
 
 		// Copy images into final one
-		$finalStartY = $heights[sizeof($heights)-1];
-		imagecopy($image, $image1, 0, 0, 0, 0, $width_1, $height_1);
-		imagecopy($image, $image2, $width_1, 0, 0, 0, $width_2, $height_2);
-		imagecopy($image, $image3, $width_1 + $width_2, 0, 0, 0, $width_3, $height_3);
-		imagecopy($image, $image4, $width_1 + $width_2 + $width_3, 0, 0, 0, $width_4, $height_4);
-
-		imagecopy($image, $image5, $wdthsLineDiff/5, $finalStartY, 0, 0, $width_5, $height_5);
-		imagecopy($image, $image6, 2*$wdthsLineDiff/5 +$width_5, $finalStartY, 0, 0, $width_6, $height_6);
-		imagecopy($image, $image7, 3*$wdthsLineDiff/5 +$width_5 + $width_6, $finalStartY, 0, 0, $width_7, $height_7);
-		imagecopy($image, $image8, 4*$wdthsLineDiff/5 +$width_5 + $width_6 + $width_7, $finalStartY, 0, 0, $width_8, $height_8);
+		for ($y=0; $y < 2; $y++) { 
+			for ($x=0; $x < 4; $x++) { 
+				imagecopy(
+					$image, 				//dest
+					$pngImages[$y*4+$x],	//src
+					$x*320,					//start_x
+					$y*640, 				//start_y
+					0,
+					0, 
+					320, 					//width  (x)
+					640						//height (y)
+				);
+			}
+		}
 
 		// Save the resulting image to disk (as PNG)
 
@@ -117,16 +58,9 @@ class ImgGenerator
 		echo "<br><br>".$imgOutput."<br><br>";
 
 		// Clean up
-
-		imagedestroy($image);
-		imagedestroy($image1);
-		imagedestroy($image2);
-		imagedestroy($image3);
-		imagedestroy($image4);
-		imagedestroy($image5);
-		imagedestroy($image6);
-		imagedestroy($image7);
-		imagedestroy($image8);
+		for ($i=0; $i < 8; $i++) { 
+			imagedestroy($pngImages[$i]);
+		}
 	}
 
 	function GetValueByIndex($index, $array) {
